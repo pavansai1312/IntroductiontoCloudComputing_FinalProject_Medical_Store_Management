c4c0588536505e2fb882197b8bf40043b93c2abd
pavansai1312 <51266110+pavansai1312@users.noreply.github.com>
2023-08-02 12:53:46 -0500

    RegistryserverApplication  code pushed

--- registryserver/src/main/java/com/medicalstoreapp/registryserver/RegistryserverApplication.java
+++ registryserver/src/main/java/com/medicalstoreapp/registryserver/RegistryserverApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 
+ * @author Pavan Sai
+ *
+ *         Main Class for Registry Server
+ */
+
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistryserverApplication {
