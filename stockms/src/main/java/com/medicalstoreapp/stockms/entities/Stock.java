6741963d999cc47143c6f3cf9a6ba8ac68d8c662
pavansai1312 <51266110+pavansai1312@users.noreply.github.com>
2023-08-02 12:51:15 -0500

    Stock Module Code pushed

--- stockms/src/main/java/com/medicalstoreapp/stockms/entities/Stock.java
+++ stockms/src/main/java/com/medicalstoreapp/stockms/entities/Stock.java
@@ -6,6 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 
+ * @author Pavan Sai
+ *
+ *         Entity Class for Stock Module
+ */
+
 @Entity
 public class Stock {
 	@GeneratedValue
